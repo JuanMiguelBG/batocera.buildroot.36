018d5b4fe5d45f91419475aa16fb94fe8ea351cd
JuanMiguelBG <bacojmbg@gmail.com>
2023-05-15 19:59:13 +0200

    Adding Batocera-linux Buildroot v36 - Commit: 116404f2807c1d5137b055f535be37ef4fbf3ff0

---
+++ package/rtl8723ds/rtl8723ds.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+# rtl8723ds
+#
+################################################################################
+# batocera (update)
+RTL8723DS_VERSION = 5ab2d3f3858dc4c833013c47a79faa05b10198f6
+RTL8723DS_SITE = $(call github,lwfinger,rtl8723ds,$(RTL8723DS_VERSION))
+RTL8723DS_LICENSE = GPL-2.0
+
+# Undefine the hardcoded CONFIG_LITTLE_ENDIAN
+RTL8723DS_USER_EXTRA_CLAGS = -UCONFIG_LITTLE_ENDIAN
+# Set endianness
+RTL8723DS_USER_EXTRA_CLAGS += -DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN
+
+RTL8723DS_MODULE_MAKE_OPTS = \
+	CONFIG_RTL8723DS=m \
+	KVER=$(LINUX_VERSION_PROBED) \
+	KSRC=$(LINUX_DIR) \
+	USER_EXTRA_CFLAGS="$(RTL8723DS_USER_EXTRA_CLAGS)"
+
+define RTL8723DS_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_NET)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_WIRELESS)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_CFG80211)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MMC)
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
---
+++ package/wlroots/wlroots.mk
@@ -0,0 +1,66 @@
+################################################################################
+#
+# wlroots
+#
+################################################################################
+# batocera (update)
+WLROOTS_VERSION = 0.16.1
+WLROOTS_SITE = https://gitlab.freedesktop.org/wlroots/wlroots/-/releases/$(WLROOTS_VERSION)/downloads
+WLROOTS_LICENSE = MIT
+WLROOTS_LICENSE_FILES = LICENSE
+WLROOTS_INSTALL_STAGING = YES
+
+WLROOTS_DEPENDENCIES = \
+	host-pkgconf \
+	host-wayland \
+	libinput \
+	libxkbcommon \
+	libegl \
+	libgles \
+	pixman \
+	seatd \
+	udev \
+    hwdata \
+	wayland \
+	wayland-protocols
+
+WLROOTS_CONF_OPTS = -Dexamples=false -Dxcb-errors=disabled
+
+WLROOTS_RENDERERS = gles2
+WLROOTS_BACKENDS = libinput drm
+
+# batocera
+ifeq ($(BR2_PACKAGE_HAS_LIBGBM),y)
+WLROOTS_CONF_OPTS += -Dallocators=gbm
+endif
+
+# batocera - add xwayland
+ifeq ($(BR2_PACKAGE_WLROOTS_X11),y)
+WLROOTS_BACKENDS += x11
+WLROOTS_CONF_OPTS += -Dxwayland=enabled
+WLROOTS_DEPENDENCIES += libxcb xcb-util-wm xcb-util-renderutil xlib_libX11 xwayland
+else
+WLROOTS_CONF_OPTS += -Dxwayland=disabled
+endif
+
+# batocera - add vulkan build dependency
+ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
+    WLROOTS_DEPENDENCIES +=  vulkan-headers vulkan-loader host-glslang
+endif
+
+ifeq ($(BR2_PACKAGE_MESA3D_VULKAN_DRIVER),y)
+WLROOTS_RENDERERS += vulkan
+WLROOTS_DEPENDENCIES += mesa3d
+endif
+
+WLROOTS_CONF_OPTS += \
+	-Dbackends=$(subst $(space),$(comma),$(strip $(WLROOTS_BACKENDS))) \
+	-Drenderers=$(subst $(space),$(comma),$(strip $(WLROOTS_RENDERERS)))
+
+# batocera
+define WLROOTS_CROSS_FIXUP
+	$(SED) 's|/usr/share/hwdata/pnp.ids|$(TARGET_DIR)/usr/share/hwdata/pnp.ids|g' $(@D)/backend/drm/meson.build
+endef
+WLROOTS_PRE_CONFIGURE_HOOKS += WLROOTS_CROSS_FIXUP
+
+$(eval $(meson-package))
